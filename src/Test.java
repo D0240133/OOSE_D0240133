c29a88355aee6a9cab6185fb8c9280ae93f8d11c
D0240133 <jid12121@gmail.com>
2015-12-21 21:50:15 +0800

    work3 Final D0240133

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,54 @@
-class Question {
-   String description;
-   public Question(String desc) {
-      this.description = desc;
-   }
-   public void print() {
-      System.out.println(desc);
-   }      
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+abstract class Component{
+	abstract void op();
 }
+
+class Question extends Component{
+	  String description;
+	  public Question(String desc) {
+	     this.description = desc;
+	   }
+	  public void print() {
+	     System.out.println(description);
+	  }  
+	
+	 ArrayList <Component> list ;
+	 public Question () { 
+		 list = new ArrayList <Component >() ; 
+	}
+	 void addComponent(Component c) { 
+		 list . add(c) ; 
+	 }
+	 void op() { 
+		 ListIterator <Component> iterator = list . listIterator () ; 
+		 while ( iterator . hasNext () ) { 
+			 Component c = iterator . next () ; 
+			 c.op() ; 
+		 }
+	} 
+
+    
+}
+class Leaf extends Component { 
+	// OPERATION in LEAF 
+	void op() { 
+		System.out.println("Test");
+	} 
+}
+	
+
+
+
 public class Test {
 
 	public static void main(String[] args) {
+		Component[] cc = new Question[10];
 		// TODO Auto-generated method stub
-
+		for (Component c: cc)
+		 c.op() ;
+		
 	}
 
 }
