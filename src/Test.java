c98a82dd33a9403284d2b4033978af2713c3b182
D0240133 <jid12121@gmail.com>
2015-12-21 21:16:14 +0800

    work 2

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,12 @@
-
+class Question {
+   String description;
+   public Question(String desc) {
+      this.description = desc;
+   }
+   public void print() {
+      System.out.println(desc);
+   }      
+}
 public class Test {
 
 	public static void main(String[] args) {
